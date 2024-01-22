906f5a348b11804bb40e797d15c2cf5fe7e08951
Fulgurance <zohran.londais@gmail.com>
2024-01-22 21:25:12 +0000

    Updated default settings

--- English/0.1.0/0.1.0.cr
+++ English/0.1.0/0.1.0.cr
@@ -1,2 +1,20 @@
 class Target < ISM::VirtualSoftware
+
+    def install
+        super
+
+        if option("Britain")
+            runLocaledefCommand(["-i","en_GB","-f","UTF-8","en_GB.UTF-8"])
+        end
+
+        if option("USA")
+            runLocaledefCommand(["-i","en_US","-f","UTF-8","en_US.UTF-8"])
+        end
+    end
+
+    def uninstall
+        super
+
+    end
+
 end
