1eb2b9743cb5c9dd132811fc1ccebb24d146f190
Fulgurance <zohran.londais@gmail.com>
2024-06-14 10:22:46 +0100

    Updated to last standard

--- English/0.1.0/0.1.0.cr
+++ English/0.1.0/0.1.0.cr
@@ -4,17 +4,17 @@ class Target < ISM::VirtualSoftware
         super
 
         if option("Britain")
-            runLocaledefCommand(["-i","en_GB","-f","UTF-8","en_GB.UTF-8"])
+            runLocaledefCommand("-i en_GB -f UTF-8 en_GB.UTF-8")
         end
 
         if option("USA")
-            runLocaledefCommand(["-i","en_US","-f","UTF-8","en_US.UTF-8"])
+            runLocaledefCommand("-i en_US -f UTF-8 en_US.UTF-8")
         end
     end
 
     def uninstall
         super
-
+        #Need to find a way to uninstall properly
     end
 
 end
