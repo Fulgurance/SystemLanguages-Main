af611d3e5288311c892c838486ca246213d39d6e
Fulgurance <zohran.londais@gmail.com>
2024-11-20 13:14:40 +0000

    Added default language

---
+++ Default/0.1.0/0.1.0.cr
@@ -0,0 +1,15 @@
+class Target < ISM::VirtualSoftware
+
+    def install
+        super
+
+        #Install a fallback language by default
+        runLocaledefCommand("-i C -f UTF-8 C.UTF-8")
+    end
+
+    def uninstall
+        super
+        #Need to find a way to uninstall properly
+    end
+
+end
